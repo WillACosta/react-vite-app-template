561c897a73a48b75168cd27b68fbba54befbc723
ACostaWill <william.costa@zup.com.br>
2024-11-28 14:43:14 -0300

    chore: add script for fix code style and change prettier settings

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import App from '../App'
 
 test('should renders the app and find count button', () => {
-	render(<App />)
-	const linkElement = screen.getByTestId('count-button')
-	expect(linkElement).toBeInTheDocument()
+  render(<App />)
+  const linkElement = screen.getByTestId('count-button')
+  expect(linkElement).toBeInTheDocument()
 })
